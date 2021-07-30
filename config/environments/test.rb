d827e1a00166d8b6eda9724bd9dc7e515a8a8199
FluffyStitch <ntkoroleva1@gmail.com>
2021-07-30 09:57:28 +0300

    edit credentials

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
-  config.secret_key_base = '<%= ENV["SECRET_TEST_KEY_BASE"] %>'
+  config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
