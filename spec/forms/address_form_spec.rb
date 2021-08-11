5db0ab0cc79df1845d4b6adb3cf6fa21a81dea9b
FluffyStitch <ntkoroleva1@gmail.com>
2021-08-11 13:58:03 +0300

    add tests

--- spec/forms/address_form_spec.rb
+++ spec/forms/address_form_spec.rb
@@ -3,31 +3,50 @@
 RSpec.describe AddressForm, type: :form do
   subject { described_class.new(BillingAddress.new) }
 
-  let(:attributes) { attributes_for(:billing_address) }
-  let(:invalid_param) { rand }
+  describe 'when params is null' do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:address) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:zip) }
+    it { is_expected.to validate_presence_of(:country) }
+    it { is_expected.to validate_presence_of(:phone) }
+    it { is_expected.to validate_presence_of(:type) }
+  end
 
-  it { is_expected.to validate_presence_of(:first_name) }
-  it { is_expected.to validate_presence_of(:last_name) }
-  it { is_expected.to validate_presence_of(:address) }
-  it { is_expected.to validate_presence_of(:city) }
-  it { is_expected.to validate_presence_of(:zip) }
-  it { is_expected.to validate_presence_of(:country) }
-  it { is_expected.to validate_presence_of(:phone) }
-  it { is_expected.to validate_presence_of(:type) }
+  describe 'when params is valid' do
+    let(:attributes) { attributes_for(:billing_address) }
 
-  it { is_expected.to allow_value(attributes[:first_name]).for(:first_name) }
-  it { is_expected.to allow_value(attributes[:last_name]).for(:last_name) }
-  it { is_expected.to allow_value(attributes[:address]).for(:address) }
-  it { is_expected.to allow_value(attributes[:city]).for(:city) }
-  it { is_expected.to allow_value(attributes[:zip]).for(:zip) }
-  it { is_expected.to allow_value(attributes[:type]).for(:type) }
+    it { is_expected.to allow_value(attributes[:first_name]).for(:first_name) }
+    it { is_expected.to allow_value(attributes[:last_name]).for(:last_name) }
+    it { is_expected.to allow_value(attributes[:address]).for(:address) }
+    it { is_expected.to allow_value(attributes[:city]).for(:city) }
+    it { is_expected.to allow_value(attributes[:zip]).for(:zip) }
+    it { is_expected.to allow_value(attributes[:type]).for(:type) }
+  end
 
-  it { is_expected.not_to allow_value(invalid_param).for(:first_name) }
-  it { is_expected.not_to allow_value(invalid_param).for(:last_name) }
-  it { is_expected.not_to allow_value(invalid_param).for(:address) }
-  it { is_expected.not_to allow_value(invalid_param).for(:city) }
-  it { is_expected.not_to allow_value(invalid_param).for(:zip) }
-  it { is_expected.not_to allow_value(invalid_param).for(:country) }
-  it { is_expected.not_to allow_value(invalid_param).for(:phone) }
-  it { is_expected.not_to allow_value(invalid_param).for(:type) }
+  describe 'when params is invalid format' do
+    let(:invalid_param) { rand }
+
+    it { is_expected.not_to allow_value(invalid_param).for(:first_name) }
+    it { is_expected.not_to allow_value(invalid_param).for(:last_name) }
+    it { is_expected.not_to allow_value(invalid_param).for(:address) }
+    it { is_expected.not_to allow_value(invalid_param).for(:city) }
+    it { is_expected.not_to allow_value(invalid_param).for(:zip) }
+    it { is_expected.not_to allow_value(invalid_param).for(:country) }
+    it { is_expected.not_to allow_value(invalid_param).for(:phone) }
+    it { is_expected.not_to allow_value(invalid_param).for(:type) }
+  end
+
+  describe 'when params is too long' do
+    let(:invalid_param) { Array.new(AddressForm::LENGTH[:first_name] + 1) { rand(10) }.join }
+
+    it { is_expected.not_to allow_value(invalid_param).for(:first_name) }
+    it { is_expected.not_to allow_value(invalid_param).for(:last_name) }
+    it { is_expected.not_to allow_value(invalid_param).for(:address) }
+    it { is_expected.not_to allow_value(invalid_param).for(:city) }
+    it { is_expected.not_to allow_value(invalid_param).for(:zip) }
+    it { is_expected.not_to allow_value(invalid_param).for(:country) }
+    it { is_expected.not_to allow_value(invalid_param).for(:phone) }
+  end
 end
